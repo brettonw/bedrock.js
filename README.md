354af1f426c0c83632ac4697b7ea9dd6bebe77b2
Bretton Wade <brettonw.wade@gmail.com>
2017-05-12 12:25:27 -0400

    working on forms

--- README.md
+++ README.md
@@ -3,3 +3,33 @@ Base Javascript code for websites.
 
  https://brettonw.github.io/bedrock.js/
  
+## Style Guide
+### Names
+- All Javascript variable names and function names should be lower-camel case. 
+- All Javascript class names should be capital-camel case.
+- All constant values should be all upper-case with underscores as word separators.
+
+#### JS:
+
+    // this is a constant
+    const MY_CONSTANT_VALUE = "hello world";
+     
+    // this is an Object declaration used as a class prototype
+    let MyClass = function () {
+        // this is a private static variable
+        let myJsVariableName = MY_CONSTANT_VALUE;
+    } ();
+    
+
+#### JS
+
+All HTML Element Ids, and CSS Class Names should be lowercase, dash separated.
+
+#### HTML:
+
+    <div id="my-div-id">blah</div>
+
+#### CSS:
+
+    .my-css-class { width: 100%; }
+    
---
+++ js/forms.js
@@ -0,0 +1,122 @@
+let Forms = function () {
+    let _ = Object.create (Bedrock.Base);
+
+    let forms = {};
+
+    const INPUT = "-input-"
+    const ERROR = "-error-"
+
+    let addElement = function (parent, tag, options) {
+        let element = document.createElement(tag);
+        let optionNames = Object.keys(options);
+        for (let optionName of optionNames) {
+            switch (optionName) {
+                case "class":{
+                    element.classList.add (options.class);
+                    break;
+                }
+                case "classes": {
+                    for (let cssClass of options.classes) {
+                        element.classList.add (cssClass);
+                    }
+                    break;
+                }
+                default: {
+                    element[optionName] = options[optionName];
+                    break;
+                }
+            }
+        }
+        parent.appendChild(element);
+        return element;
+    };
+
+    _.init = function (parameters) {
+        // parameters.name - name of the form (and the event to use when submitting)
+        let formName = this.name = parameters.name;
+        forms[formName] = this;
+
+        // parameters.div - where to put the form
+        let divElement = document.getElementById(parameters.div);
+
+        // parameters.inputs - array of input names, with prompt, type, default value, template, and required flag
+        // input = { name: "nm", type: "text|checkbox|select", label: "blah", required: true, (...values appropriate for input type...) }
+        let inputs = this.inputs = {};
+        for (let input of parameters.inputs) {
+            /*
+             <div class="form-div">
+             <div class="form-title-div">Device Id:</div>
+             <input blah blah blah">
+             <div id="input-device-id-error" class="form-error">REQUIRED</div>
+             </div>
+             */
+
+            // create the div and set the title
+            let formDivElement = addElement (divElement, "div", { class: "form-div" });
+            let formTitleDivElement = addElement (formDivElement, "div", { class: "form-title-div", innerHTML: input.label });
+
+            // now add the actual input
+            let inputElement;
+            switch (input.type) {
+                case "text": {
+                    /*
+                     <input class="form-input" id="input-device-id" type="text" placeholder="Enter your Device ID">
+                     */
+                    inputElement = addElement (formDivElement, "input", { id: (formName + INPUT + input.name), type: "text", class: "form-input", placeholder: input.placeholder });
+                    break;
+                }
+                case "checkbox": {
+                    /*
+                     <input class="form-input" id="input-enable" type="checkbox" checked="checked">
+                     */
+                    inputElement = addElement (formDivElement, "input", { id: (formName + INPUT + input.name), type: "checkbox", class: "form-input", checked: input.checked });
+                    break;
+                }
+                case "select": {
+                    // XXX I ultimately want this to be a dropdown list with a type-able value box
+                    /*
+                     <select class="form-input" id="input-age-rating">
+                     <option value="">UNRATED</option>
+                     ...
+                     <option value="17">MPAA NC-17</option>
+                     </select>
+                     */
+                    inputElement = addElement (formDivElement, "select", { id: (formName + INPUT + input.name), class: "form-input" });
+                    for (let option of input.options) {
+                        let optionElement = addElement (inputElement, "option", { value: option.value, label: option.label });
+                        if ("selected" in option) {
+                            optionElement.selected = option.selected;
+                        }
+                    }
+                    break;
+                }
+            }
+
+            // and now add the error
+            let errorElement = addElement (formDivElement, "div", { id: (formName + ERROR + input.name), class: "form-error", innerHTML: "REQUIRED" });
+
+            // and fill the inputs object
+            inputs[input.name] = {
+                name: input.name,
+                type: input.type,
+                required: ("required" in input) ? input.required : false,
+                inputElement: inputElement,
+                errorElement: errorElement
+            };
+        }
+
+        // now add the submit button
+        /*
+         <div class="form-div form-button-wrapper">
+         <input class="bedrockClearButton" type="button" value="SUBMIT" onclick="onClickSubmitApplicationPolicy ();">
+         </div>
+         */
+        let formDivElement = addElement (divElement, "div", { classes: ["form-div", "form-button-wrapper"] });
+        addElement (formDivElement, "input", { type: "button", value: "SUBMIT", class: "bedrockClearButton" });
+
+        return this;
+    };
+
+    return _;
+} ();
+
