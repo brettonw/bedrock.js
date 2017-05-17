a13bcea3ed34172580fbc3c260bd82d038028fe0
Bretton Wade <brettonw.wade@gmail.com>
2017-05-16 23:40:22 -0400

    add a little bit of fetch capability

--- js/bedrock.js
+++ js/bedrock.js
@@ -1,6 +1,28 @@
 let Bedrock = function () {
     let $ = Object.create (null);
 
+    $.get = function (queryString, onSuccess) {
+        let request = new XMLHttpRequest ();
+        request.overrideMimeType ("application/json");
+        request.open ("GET", queryString, true);
+        request.onload = function (event) {
+            if (request.status === 200) {
+                let response = JSON.parse (this.responseText);
+                onSuccess (response);
+            }
+        };
+        request.send ();
+    };
+
+    $.getFromServiceBase = function (queryString, onSuccess) {
+        this.fetch (queryString, function (response) {
+            console.log (form.name + " (status: " + response.status + ")");
+            if (response.status === "ok") {
+                onSuccess (response.response);
+            }
+        });
+    };
+
     $.Base = function () {
         let _ = Object.create (null);
 
